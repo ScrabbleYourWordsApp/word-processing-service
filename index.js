d01ec3fa19f8ac92386c4ce75e3c8e8f857e7fa7
Benjamin Dendas <benjamin.dendas@craftworkz.be>
2020-04-12 13:00:12 +0200

    SYWA-12: toevoegen linter

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || '8080';
 
-app.get("/", (req, res) => {
-    res.status(200).send("Word Processing Service is up and running!");
-    });
+app.get('/', (req, res) => {
+  res.status(200).send('Word Processing Service is up and running!');
+});
 
 app.listen(port, () => {
-     console.log(`Listening to requests on http://localhost:${port}`);
-    });
-    
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
